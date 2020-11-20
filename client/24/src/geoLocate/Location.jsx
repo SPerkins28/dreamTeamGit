dcb43d9f2d372ba058638e61311d37821b7dad3f
Jillian Howell <40151421+jilherma@users.noreply.github.com>
2020-11-20 10:15:46 -0500

    changed weather

--- client/24/src/geoLocate/Location.jsx
+++ client/24/src/geoLocate/Location.jsx
@@ -1,35 +1,19 @@
-import React from "react";
-import { geolocated } from "react-geolocated";
- 
-class Demo extends React.Component {
-    render() {
-        return !this.props.isGeolocationAvailable ? (
-            <div>Your browser does not support Geolocation</div>
-        ) : !this.props.isGeolocationEnabled ? (
-            <div>Geolocation is not enabled</div>
-        ) : this.props.coords ? (
-            <table>
-                <tbody>
-                    <tr>
-                        <td>latitude</td>
-                        <td>{this.props.coords.latitude}</td>
-                    </tr>
-                    <tr>
-                        <td>longitude</td>
-                        <td>{this.props.coords.longitude}</td>
-                    </tr>
-                    
-                </tbody>
-            </table>
-        ) : (
-            <div>Getting the location data&hellip; </div>
-        );
-    }
-}
- 
-export default geolocated({
-    positionOptions: {
-        enableHighAccuracy: false,
-    },
-    userDecisionTimeout: 5000,
-})(Demo);
+import React from 'react';
+import { usePosition } from 'use-position';
+
+const Location = () => {
+    const watch = true;
+    const {
+      latitude,
+      longitude,
+    } = usePosition(watch);
+
+    return (
+      <div className="location">
+          <h3>You Be Here</h3>
+        <h5 className="changePlease">latitude: {latitude}</h5>
+        <h5 className="changePlease">longitude: {longitude}</h5>
+      </div>
+    );
+};
+  export default Location;
