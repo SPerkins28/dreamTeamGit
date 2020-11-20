1f95f19553e911b7e8bb4815cf244c38a2f9506c
lizafortozo <lizafortozo@gmail.com>
2020-11-20 09:53:07 -0500

    final

---
+++ client/24/src/Componets/NASA/Picture.jsx
@@ -0,0 +1,22 @@
+/*
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Picture = (props) => {
+    return ()
+
+    
+};
+
+
+
+
+/*
+export default function Home (){
+    return (
+        <div className= "home">
+            <Link class name = "home-link"  to ="/NasaPhoto">Take a Look!</Link>
+        </div>
+    );
+ }
+ */
